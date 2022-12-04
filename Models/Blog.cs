095bd178122c74d131e72d3066ab09ba730c3549
lcsmota <sacul_m@protonmail.com>
2022-12-03 21:17:46 -0300

    Add Blog model

---
+++ Models/Blog.cs
@@ -0,0 +1,9 @@
+namespace BlogApi.Models;
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; } = string.Empty;
+
+    public List<Post> Posts { get; set; } = new();
+}
