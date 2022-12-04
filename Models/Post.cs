d41014235210828617a187dc1b3bd417ad401970
lcsmota <sacul_m@protonmail.com>
2022-12-03 21:17:35 -0300

    Add Post model

---
+++ Models/Post.cs
@@ -0,0 +1,12 @@
+namespace BlogApi.Models;
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
