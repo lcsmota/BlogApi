d3292324a84e33d0d74e2efb5eaf49e243e15c29
lcsmota <sacul_m@protonmail.com>
2022-12-04 21:14:47 -0300

    Add Swagger Doc

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using BlogApi.Context;
 using BlogApi.Interfaces;
 using BlogApi.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -16,7 +17,15 @@ var builder = WebApplication.CreateBuilder(args);
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(sw =>
+    {
+        sw.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Blog Api",
+            Version = "v1",
+            Description = "Simple CRUD using Entity Framework 6"
+        });
+    });
 }
 
 var app = builder.Build();
